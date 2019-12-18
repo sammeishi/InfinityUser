fd8427d3bac6be749bc43a60264d5e5bab058a31
sam <sammeishi@gmail.com>
2019-12-18 09:51:25 +0800

    asArray默认只包含profile零件，其他零件可以传入.

--- src/ext/ToolExt.trait.php
+++ src/ext/ToolExt.trait.php
@@ -10,20 +10,20 @@ use yii\helpers\ArrayHelper;
 trait ToolExt{
     /**
      * 将User实例转换为数组.
-     * 先将User自身转换为数组，然后遍历各个零件，转换为数组类型嵌入到User上.
-     * @param array | null $specify 仅允许的零件被转换为数组，null为全部零件
+     * 默认包含零件,可以追加指定零件
+     * @param array | null $specify 追加的零件
      * @return array
      */
     public function asArray( $specify = null ){
-        //要转换的零件列表,此处定义的零件都在User上对应了getter,不定义会报错
-        $partList = ['profile','loginBook'];
+        $specify = ($specify && !is_array( $specify )) ?  [ $specify ] : $specify;
+        $specify = $specify ? $specify : [];
+        //此处定义的零件都在User上对应了getter,不定义会报错
+        $partList = array_merge( ['profile']  ,$specify );
         //先转换自身
         $res = ArrayHelper::toArray($this);
         //遍历获取零件，会触发sql查询！然后转换为Array
         foreach ( $partList as $partName ){
-            if( !$specify || isset( $specify[ $partName ] ) ){
-                $res[ $partName ] = ArrayHelper::toArray($this->{$partName});
-            }
+            $res[ $partName ] = ArrayHelper::toArray($this->{$partName});
         }
         return $res;
     }
